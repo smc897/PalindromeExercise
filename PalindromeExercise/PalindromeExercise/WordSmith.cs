9a5458f59c66ad42c3bc2386809d9c228c2c22c4
ScottMC89 <scottmc592@gmail.com>
2023-10-21 12:24:19 -0500

    done.

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(String input) {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            String inputReversed = new string(chars);
+            if (input == inputReversed) return true;
+            else return false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
+using PalindromeExercise;
 using System;
 using Xunit;
+using Xunit.Sdk;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("tacocat",true)]
+        [InlineData("taco",false)]
+        [InlineData("racecar",true)]
+        public void PalindromeTest(String input, bool expected)
         {
+            //arrange
+            WordSmith ws = new WordSmith();
 
+            //act
+            var actual = ws.IsAPalindrome(input);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
